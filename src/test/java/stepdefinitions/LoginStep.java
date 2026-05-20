fde77e87b4f30f47c8c49fb9c586c21eed978048
RENDI\rendy <rendy.eduardi@gmail.com>
2026-05-20 15:19:43 +0700

    test lagi

--- src/test/java/stepdefinitions/LoginStep.java
+++ src/test/java/stepdefinitions/LoginStep.java
@@ -49,7 +49,7 @@ public class LoginStep {
 
     @When("User enters username {string}")
     public void user_enters_username(String username) {
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("user-name"))).sendKeys(username) //test
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("user-name"))).sendKeys(username) //test lagi
     }
 
     @And("User enters password {string}")
