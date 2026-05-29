4ee070a1253323140e50d111199f3c44c1d73ceb
RENDI\rendy <rendy.eduardi@gmail.com>
2026-05-29 17:43:48 +0700

    update step definition hook

---
+++ src/test/java/stepdefinitions/Hooks.java
@@ -0,0 +1,46 @@
+package stepdefinitions;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Hooks {
+
+    public static WebDriver driver;
+    public static WebDriverWait wait;
+
+    @Before
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+
+        options.addArguments("--disable-notifications");
+        options.addArguments("--disable-infobars");
+        options.addArguments("--disable-save-password-bubble");
+        options.addArguments("--disable-features=PasswordManagerEnabled");
+        options.addArguments("--guest");
+
+        Map<String, Object> prefs = new HashMap<>();
+        prefs.put("credentials_enable_service", false);
+        prefs.put("profile.password_manager_enabled", false);
+        prefs.put("profile.password_manager_leak_detection", false);
+        options.setExperimentalOption("prefs", prefs);
+
+        driver = new ChromeDriver(options);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+    }
+
+    @After
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
